cc6a6c905e7211fe86bfd0a4e1696ed599870f85
Kevin Xue <kevin7282@hotmail.com>
2023-02-16 15:58:02 -0500

    change to prod-378015

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/')
+def welcome():
+  return "Welcome to the blue deployment"
+
 @app.route("/hello")
 def hello_world():
     return "Hello, World! Blue deployment"
